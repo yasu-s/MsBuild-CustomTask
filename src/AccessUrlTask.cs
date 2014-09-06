746895b70de5490d20e288a0d52652bb8a307850
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2014-09-06 14:18:41 +0900

    HTTP成功判定修正

--- src/AccessUrlTask.cs
+++ src/AccessUrlTask.cs
@@ -37,7 +37,7 @@
 
                 Console.WriteLine("HTTP STATUS: {0}", code);
 
-                result = (code == HttpStatusCode.OK);
+                result = (((int)HttpStatusCode.OK <= (int)code) && ((int)code <= (int)HttpStatusCode.MultipleChoices));
             }
             catch (System.Exception ex)
             {
